33b3eec9d93412ef76c97a5fff0407d34e3ed51b
Kevin Hernandez <kevinalbertohernandezgarcia@gmail.com>
2026-03-24 20:41:16 -0500

    Initial Commit

---
+++ AnalyzeSentiment/AnalyzeSentimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyzeSentimentApp.swift
+//  AnalyzeSentiment
+//
+//  Created by Kevin Hernandez Garcia on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct AnalyzeSentimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
